961aa9a58e962ee52b03a6229231217f34d65b5b
Eduardo Henrique Hoffmann <edu.he.dev@gmail.com>
2025-12-11 02:41:41 -0300

    refactor: refactor method draw

--- src/sprites/food.py
+++ src/sprites/food.py
@@ -1,11 +1,30 @@
+import random
+
+
 class Food:
-    def __init__(self):
-        
+
+    def __init__(self, canva, screen_width, screen_height):
+
+        self.food_size = 10
+        self.canva = canva
+        self.food_color = 'red'
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+        self.coordinates = []
+        self.draw()
+
+    def draw(self):
+
         x = random.randint(0,
-        (WIDTH / SPACE_SIZE)-1) * SPACE_SIZE
-        y = random.randint(0, (HEIGHT / SPACE_SIZE)-1)* SPACE_SIZE
+                           (self.screen_width // self.food_size) - 1) * self.food_size
 
-        self.coordinates = [x, y]
+        y = random.randint(
+            0, (self.screen_height // self.food_size)-1) * self.food_size
 
-    def draw():
-        canva.create_oval(x, y, z + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD, tag="food")
+        self.canva.create_oval(x, y, x + self.food_size,
+                               y + self.food_size, fill=self.food_color, tag="food")
+
+        self.coordinates = (x, y)
+
+    def delet_food(self):
+        self.canva.delete("food")
